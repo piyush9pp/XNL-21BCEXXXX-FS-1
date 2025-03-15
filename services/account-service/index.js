5ff971eb773b5a2f6662efaa27472b6dfa35971b
piyush9pp <152002428+piyush9pp@users.noreply.github.com>
2025-03-15 17:42:24 +0530

    Update index.js

--- services/account-service/index.js
+++ services/account-service/index.js
@@ -1,4 +1,4 @@
-// services/user-service/index.js
+
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 1. Connect to MongoDB
+
 const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/account-service-db";
 console.log(process.env.MONGO_URI);
@@ -23,7 +23,7 @@ mongoose
     console.error("Account Service MongoDB connection error:", err)
   );
 
-// 2. Define a Transaction schema and model
+
 const transactionSchema = new mongoose.Schema({
   id: String,
   fromUser: String,
@@ -35,7 +35,7 @@ const transactionSchema = new mongoose.Schema({
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
-// Endpoint to store transactions
+
 app.post("/transactions", async (req, res) => {
   try {
     const transactionData = req.body;
