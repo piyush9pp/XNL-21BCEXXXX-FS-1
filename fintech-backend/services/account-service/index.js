1396866edcbc5deee48e448ce6ffa6c45f667efd
piyush9pp <152002428+piyush9pp@users.noreply.github.com>
2025-03-15 17:39:19 +0530

    Update index.js

--- fintech-backend/services/account-service/index.js
+++ fintech-backend/services/account-service/index.js
@@ -1,4 +1,3 @@
-// services/user-service/index.js
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -8,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 1. Connect to MongoDB
+
 const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/account-service-db";
 console.log(process.env.MONGO_URI);
@@ -23,7 +22,7 @@ mongoose
     console.error("Account Service MongoDB connection error:", err)
   );
 
-// 2. Define a Transaction schema and model
+
 const transactionSchema = new mongoose.Schema({
   id: String,
   fromUser: String,
@@ -35,7 +34,7 @@ const transactionSchema = new mongoose.Schema({
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
-// Endpoint to store transactions
+
 app.post("/transactions", async (req, res) => {
   try {
     const transactionData = req.body;
